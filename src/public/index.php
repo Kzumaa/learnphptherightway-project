7af03b711b767cef135d15a8c94cf157c6c833b3
Duy <vuducduy1012@gmail.com>
2024-08-10 11:38:42 +0700

    Composer installed

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,33 @@
 <?php
 
-declare(strict_types = 1);
+// require_once '../PaymentGateway/Paddle/Transaction.php';
+// require_once '../PaymentGateway/Stripe/Transaction.php';
 
-phpinfo();
+// spl_autoload_register(
+//     function($path){
+//         $path = __DIR__  . '/../' . lcfirst(str_replace('\\','/',$path)) . '.php';
+//         if (file_exists(require $path)){
+//             require $path;
+//         }
+//     }
+// );
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// spl_autoload_register(
+//     function ($class) {
+//         var_dump($class);
+//     }
+// , true);
+
+use App\PaymentGateway\Paddle\Transaction;
+
+$paddleTransaction = new Transaction();
+
+$id = new \Ramsey\Uuid\UuidFactory();
+
+echo $id->uuid4();
+
+var_dump($paddleTransaction);
+
+// And some Destruct method's usecase
